0d2ffa3f1726a4bd82994df9077564f4a74844b9
kmg <deepestdark@gmail.com>
2026-01-07 13:50:49 +0900

    add file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import {ButtonBox, ImageBox, ItemBox} from "@components/Containers"
+import {SolidLine} from "@components/Liner"
 
 export default function App() {
 
   return (
     <>
+      <ItemBox>
+        <ButtonBox></ButtonBox>
+      </ItemBox>
+      <SolidLine />
       <ItemBox>
         <ButtonBox></ButtonBox>
       </ItemBox>
---
+++ src/components/Liner.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const LineSoild = styled.div`
+    border: 1px solid black;
+`
+
+export function SolidLine(){
+    return <LineSoild />;
+}
